e4f91c3f42f4467008428ebda1ef0c3715440fa9
Aidan Bauer <baueraidan@gmail.com>
2021-05-24 15:54:34 -0400

    Load model

--- test.py
+++ test.py
@@ -12,6 +12,76 @@ from utils.common import tensor2im
 from models.psp import pSp
 from models.e4e import e4e
 
-print('Hello, world')
-
 experiment_type = 'ffhq_encode' #@param ['ffhq_encode', 'cars_encode', 'church_encode', 'horse_encode', 'afhq_wild_encode', 'toonify']
+
+EXPERIMENT_DATA_ARGS = {
+  "ffhq_encode": {
+    "model_path": "pretrained_models/restyle_psp_ffhq_encode.pt",
+    "image_path": "notebooks/images/face_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+  "cars_encode": {
+    "model_path": "pretrained_models/restyle_psp_cars_encode.pt",
+    "image_path": "notebooks/images/car_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((192, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+  "church_encode": {
+    "model_path": "pretrained_models/restyle_psp_church_encode.pt",
+    "image_path": "notebooks/images/church_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+  "horse_encode": {
+    "model_path": "pretrained_models/restyle_e4e_horse_encode.pt",
+    "image_path": "notebooks/images/horse_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+  "afhq_wild_encode": {
+    "model_path": "pretrained_models/restyle_psp_afhq_wild_encode.pt",
+    "image_path": "notebooks/images/afhq_wild_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+  "toonify": {
+    "model_path": "pretrained_models/restyle_psp_toonify.pt",
+    "image_path": "notebooks/images/toonify_img.jpg",
+    "transform": transforms.Compose([
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  },
+}
+
+EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[experiment_type]
+
+# Load pretrained model
+model_path = EXPERIMENT_ARGS['model_path']
+ckpt = torch.load(model_path, map_location='cpu')
+
+opts = ckpt['opts']
+pprint.pprint(opts)
+# Update the training options
+opts['checkpoint_path'] = model_path
+
+opts = Namespace(**opts)
+if experiment_type == 'horse_encode': 
+  net = e4e(opts)
+else:
+  net = pSp(opts)
+    
+net.eval()
+net.cuda()
+print('Model successfully loaded!')
